aea7640881d61ecc0867977d06aa67d5b42c0ceb
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-03-27 14:29:21 +0000

    [Add]top index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def new
+
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+
   end
 
   def edit
+
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   get '/' => 'homes#top'
+  get 'books/new'
+  get '/books' => 'books#index'
+  get '/books/show'
+  get 'books/edit'
+  post '/books' => 'books#create'
+  # resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
