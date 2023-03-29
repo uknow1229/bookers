1721cefa30a6c700057b0d29916381053b1db8b3
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-03-29 15:59:22 +0000

    [Add]show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,20 +6,27 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def edit
-
+     @book = Book.find(params[:id])
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/show'
+    redirect_to book_path(book.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get '/' => 'homes#top'
   get 'books/new'
   get '/books' => 'books#index'
-  get '/books/show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show',as:'book'
+  get 'books/:id/edit' => 'books#edit',as:'edit_book'
   post '/books' => 'books#create'
   # resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
