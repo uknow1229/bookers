e3521e221da625b83e4e03c58aea0dc23b6391f2
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-04-04 06:58:14 +0000

    [Add]validations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def new
-
+    @book = Book.new
   end
 
   def index
@@ -19,13 +19,31 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        flash[:notice] = "Book was successfully updated."
+        redirect_to book_path(@book.id)
+      else
+        render :edit
+      end
   end
 
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path(book.id)
   end
 
   private
